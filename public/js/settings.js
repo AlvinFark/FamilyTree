66485c6788223e81be060513fc2187bf232aaca9
Alvin Farkhan <alvin.farkhan@gmail.com>
2019-02-04 21:06:48 +0700

    [alvin] settings upload photo feature

--- public/js/settings.js
+++ public/js/settings.js
@@ -2,6 +2,31 @@ var account=true, people=false;
 $("#addPeople").hide();
 $('.btnSmall').hide();
 
+var reader = new FileReader();
+function readURL(input,id) {
+  if (input.files && input.files[0]) {
+    reader.onload = function(e) {
+      $(id).css({'background-image':'url("'+ e.target.result +'")'})
+      // $(id).attr('src', e.target.result);
+    }
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$("#accountPhotoInput").change(function() {
+  readURL(this,"#imgChangeAvatar");
+});
+$("#peoplePhotoInput").change(function() {
+  readURL(this,"#changeImgPeople");
+});
+
+$( document ).on("click","#uploadAvatar",function () {
+  $('#accountPhotoInput').trigger('click');
+})
+$( document ).on("click","#uploadPeoplePhoto",function () {
+  $('#peoplePhotoInput').trigger('click');
+})
+
 $( document ).on("click","#accountTab",function () {
   $("#addPeople").hide();
   $("#saveAccount").fadeIn();
