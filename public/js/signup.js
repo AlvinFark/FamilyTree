bf98e87d8b3466bbdff23c0559463af3752d27c0
Alvin Farkhan <alvin.farkhan@gmail.com>
2019-02-08 09:22:16 +0700

    [alvin] signup form validation

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,3 +1,5 @@
+$(".alert").hide();
+
 var match = false;
 var check = function() {
   if ((document.getElementById('inputPassword').value==document.getElementById('inputPassword2').value)
@@ -12,9 +14,25 @@ var check = function() {
   }
 }
 
-
+var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 $( document ).on("click","#btnSignUp",function () {
-  if (match) {
+  if (($("#inputEmail").val()=="")||($("#inputPassword").val()=="")||($("#inputPassword2").val()=="")) {
+    $("#alertSignUp").html("Please make sure you have filled all the forms");
+    $("#alertSignUp").fadeIn();
+    setTimeout(function () { $("#alertSignUp").fadeOut(); }, 2000);
+  }  else if (!($("#inputEmail").val().match(re))){
+    $("#alertSignUp").html("Please make sure the email you've entered is valid");
+    $("#alertSignUp").fadeIn();
+    setTimeout(function () { $("#alertSignUp").fadeOut(); }, 2000);
+  }  else if (!match) {
+    $("#alertSignUp").html("Please make sure the password and password validation have the same text");
+    $("#alertSignUp").fadeIn();
+    setTimeout(function () { $("#alertSignUp").fadeOut(); }, 2000);
+  } else if ($("#cbxTerms").prop('checked', false)) {
+    $("#alertSignUp").html("Please make sure you have agreed to the terms and conditions");
+    $("#alertSignUp").fadeIn();
+    setTimeout(function () { $("#alertSignUp").fadeOut(); }, 2000);
+  } else {
     var jsonSignUp = {
       "username" : $("#inputEmail").val(),
       "password" : $("#inputPassword").val()
