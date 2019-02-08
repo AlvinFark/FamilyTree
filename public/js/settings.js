9c37cd69838ee153ba20b8dcea9eb702010cb8a3
aftermath089 <vincentmichael089@gmail.com>
2019-02-08 20:35:54 +0800

    [vincent] settings  addPerson, 500 error from server

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,33 +1,36 @@
-var account=true, people=false;
+var account = true, people = false;
 $("#addPeople").hide();
 $('.btnSmall').hide();
-
+$(".alert").hide();
 var reader = new FileReader();
-function readURL(input,id) {
+function readURL(input, id) {
   if (input.files && input.files[0]) {
-    reader.onload = function(e) {
-      $(id).css({'background-image':'url("'+ e.target.result +'")'})
+    reader.onload = function (e) {
+      $(id).css({ 'background-image': 'url("' + e.target.result + '")' })
       // $(id).attr('src', e.target.result);
     }
     reader.readAsDataURL(input.files[0]);
   }
 }
 
-$("#accountPhotoInput").change(function() {
-  readURL(this,"#imgChangeAvatar");
+
+
+
+$("#accountPhotoInput").change(function () {
+  readURL(this, "#imgChangeAvatar");
 });
-$("#peoplePhotoInput").change(function() {
-  readURL(this,"#changeImgPeople");
+$("#peoplePhotoInput").change(function () {
+  readURL(this, "#changeImgPeople");
 });
 
-$( document ).on("click","#uploadAvatar",function () {
+$(document).on("click", "#uploadAvatar", function () {
   $('#accountPhotoInput').trigger('click');
 })
-$( document ).on("click","#uploadPeoplePhoto",function () {
+$(document).on("click", "#uploadPeoplePhoto", function () {
   $('#peoplePhotoInput').trigger('click');
 })
 
-$( document ).on("click","#addPeople",function () {
+$(document).on("click", "#addPeople", function () {
   $('#deletePerson').hide();
   $('#editPersonFirstName').val('');
   $('#editPersonLastName').val('');
@@ -36,32 +39,90 @@ $( document ).on("click","#addPeople",function () {
   $('#editPersonAddress').val('');
   $('#editPersonDateOfBirth').val('');
 })
-$( document ).on("click",".cardPerson",function () {
+
+localStorage.setItem('idcounter', 100);
+
+$(document).on("click", "#saveEditPerson", function () {
+
+  if ($('#editPersonFirstName').val() == "" || $('#editPersonLastName').val() == "" ||
+    $('#editPersonHomeNumber').val() == "" || $('#editPersonPhoneNumber').val() == "" ||
+    $('#editPersonAddress').val() == "" || $('#editPersonDateOfBirth').val() == ""
+  ) {
+    $("#alertSettingsModal").show();
+    $("#alertSettingsModal").html("Please make sure you have filled all the forms");
+    $("#alertSettingModal").fadeIn();
+    setTimeout(function () { $("#alertSettingsModal").fadeOut(); }, 2000);
+  } else {
+    var d = new Date();
+    var isod = d.toISOString();
+    var jsonAddPerson = {
+        "id" : localStorage.getItem("idcounter"),
+        "nama" : $('#editPersonFirstName').val()+" "+$('#editPersonLastName').val(),
+        "no_telp" :$('#editPersonHomeNumber').val() ,
+        "alamat" :  $('#editPersonAddress').val(),
+        "tempat_lahir" : "",
+        "tanggal_lahir" :  $('#editPersonDateOfBirth').val() ,
+        "created_at" : isod,
+        "updated_at" : isod,
+        "avatar" : {
+          "url" : "",
+        },
+        "id_pasangan" : "", 
+        "id_ortu" : "",
+        "jenis_kelamin" : $('#editPersonGender').val()
+
+    }
+    $.ajax({
+      type: "POST",
+      url: "https://silsilah-app.herokuapp.com/public/api/v1/create-person",
+      contentType: 'application/json',
+      data: JSON.stringify(jsonAddPerson),
+      beforeSend : function(xhr){
+        xhr.setRequestHeader("Authorization", localStorage.getItem('token') );
+      },
+      success: function (e) {
+        console.log("add person:  success");
+        var counter = localStorage.getItem('idcounter');
+        counter++;
+        localStorage.setItem('idcounter', counter);
+      },
+      error: function (e) {
+        $("#alertSettingsModal").show();
+        $("#alertSettingsModal").html("Somethings wrong :( "+"["+e.statusText+"]");
+        $("#alertSettingModal").fadeIn();
+        console.log(e);
+        setTimeout(function () { $("#alertSettingsModal").fadeOut(); }, 2000);
+      }
+    })
+  }
+})
+
+$(document).on("click", ".cardPerson", function () {
   $('#deletePerson').show();
 })
 
-$( document ).on("click","#accountTab",function () {
+$(document).on("click", "#accountTab", function () {
   $("#addPeople").hide();
   $("#saveAccount").fadeIn();
-  account=true;
-  people=false;
+  account = true;
+  people = false;
 })
-$( document ).on("click","#peopleTab",function () {
+$(document).on("click", "#peopleTab", function () {
   $("#saveAccount").hide();
   $("#addPeople").fadeIn();
-  account=false;
-  people=true;
+  account = false;
+  people = true;
 })
-$(document).scroll(function() {
+$(document).scroll(function () {
   var y = $(this).scrollTop();
-  if (account){
+  if (account) {
     if (y > 100) {
       $('#saveAccountSmall').fadeIn();
     } else {
       $('#saveAccountSmall').fadeOut();
     }
   };
-  if (people){
+  if (people) {
     if (y > 100) {
       $('#addPeopleSmall').fadeIn();
     } else {
@@ -69,3 +130,4 @@ $(document).scroll(function() {
     }
   };
 });
+
